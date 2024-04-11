bfbf7060dbb04ea9e195f4348ab132f5c9410fa3
Sameer Paudel <83913671+Sameer292@users.noreply.github.com>
2024-04-11 22:21:26 +0545

    Update script.js
    fixed a copy bug

--- Color_palette_generator/script.js
+++ Color_palette_generator/script.js
@@ -3,16 +3,16 @@ let generate = document.querySelector("#gen");
 let clear = document.querySelector("#clr");
 let output = document.querySelector(".outputs");
 let type = document.querySelector("#colorFormat");
-let copy=document.getElementById("copy");
-let colorRGB=[];
-let colorHEX=[];
+let copy = document.getElementById("copy");
+let colorRGB = [];
+let colorHEX = [];
 
 generate.addEventListener("click", () => {
   while (output.hasChildNodes()) {
     output.removeChild(output.firstChild);
   }
-  colorHEX=[];
-  colorRGB=[];
+  colorHEX = [];
+  colorRGB = [];
   if (
     (screen.width < 800 && numberOfcolors.value > 8) ||
     numberOfcolors.value > 15
@@ -23,21 +23,22 @@ generate.addEventListener("click", () => {
   } else {
     for (let i = 0; i < numberOfcolors.value; i++) {
       const out = document.createElement("div");
-
       if (type.value == "hex") {
         let finColor = genColorHEX();
         out.style.backgroundColor = `#${finColor}`;
         out.innerText = `#${finColor}`;
-        colorHEX+=`#${finColor}\n`;
-    } else if (type.value == "rgb") {
+        colorHEX += `#${finColor}\n`;
+      } else if (type.value == "rgb") {
         let finColor = genColorRGB();
         out.style.backgroundColor = `rgb(${finColor})`;
         out.innerText = `rgb\n(${finColor})`;
-        colorRGB+=`(${finColor})\n`;
+        colorRGB += `(${finColor})\n`;
       }
       document.querySelector(".outputs").appendChild(out);
     }
-    document.getElementById("copy").style.display = "inline";
+    if (output.hasChildNodes()) {
+      document.getElementById("copy").style.display = "inline";
+    }
   }
 });
 
@@ -60,20 +61,16 @@ const genColorHEX = () => {
   let red = "";
   let green = "";
   let blue = "";
-
   let red1;
   let green1;
   let blue1;
-
   red1 = Math.floor(Math.random() * 256);
   green1 = Math.floor(Math.random() * 256);
   blue1 = Math.floor(Math.random() * 256);
-
   red = hexFormat(red1.toString(16));
   green = hexFormat(green1.toString(16));
   blue = hexFormat(blue1.toString(16));
   result = red + green + blue;
-
   return result;
 };
 
@@ -81,7 +78,6 @@ const genColorRGB = () => {
   let red1;
   let green1;
   let blue1;
-
   red1 = Math.floor(Math.random() * 256);
   green1 = Math.floor(Math.random() * 256);
   blue1 = Math.floor(Math.random() * 256);
@@ -89,12 +85,10 @@ const genColorRGB = () => {
   return result;
 };
 
-copy.addEventListener("click",()=>{
-
-    if(type.value == "rgb"){
-        navigator.clipboard.writeText(colorRGB);
-    }
-    else{
-        navigator.clipboard.writeText(colorHEX);
-    }
+copy.addEventListener("click", () => {
+  if (type.value == "rgb") {
+    navigator.clipboard.writeText(colorRGB);
+  } else {
+    navigator.clipboard.writeText(colorHEX);
+  }
 });
