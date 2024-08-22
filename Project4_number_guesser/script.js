22bf658f14a743ae5e4fbfb2c2533abae0d3749d
Sameer Paudel <paudelsameer888@gmail.com>
2024-08-22 20:41:56 +0545

    Optimised. added restart button

--- Project4_number_guesser/script.js
+++ Project4_number_guesser/script.js
@@ -1,44 +1,67 @@
 const randVal = Math.floor(Math.random() * 100 + 1);
-
 const submit = document.querySelector("button");
 const input = document.querySelector("#inp");
 let lives = 10;
 const wrongguess = document.querySelector(".wrongguess");
+const restart = document.querySelector(".restart");
 
 document.querySelector(".rem").innerHTML = lives;
 submit.addEventListener("click", () => {
   if (lives <= 0) {
     gameOver();
   } else {
-    if (parseInt(input.value) > 100 || parseInt(input.value) < 0) {
+    if (isInputinvalid()) {
       alert("Please enter a number between 1-100");
     } else if (parseInt(input.value) === randVal) {
       alert("Congrats number matched");
-      location.reload()
+      location.reload();
     } else {
-      lives--;
-      document.querySelector(".rem").innerHTML = lives;
-      const wrongGuesses = document.createElement("div");
-      wrongGuesses.innerText = input.value;
-      wrongGuesses.classList.add("guesses");
-      wrongguess.appendChild(wrongGuesses);
-
-      if(lives>0){
-
-          if (parseInt(input.value) < randVal) {
-              alert("Incorrect! Guess higher");
-            } else {
-                alert("Incorrect! Guess lower");
-            }
-      }
-      else{
-        gameOver();
-      }
+      wrongguesser();
     }
   }
+});
+
+restart.addEventListener("click", () => {
+  location.reload();
 });
 
 function gameOver() {
   alert("Game over!!! You lose");
   location.reload();
+}
+
+function isInputinvalid() {
+  if (parseInt(input.value) > 100 || parseInt(input.value) < 0 || isNaN(parseInt(input.value))) {
+    return true;
+  }
+}
+
+function wrongguesser() {
+  lifesucker();
+  displayprevguesses();
+
+  if (lives > 0) {
+    highorlow();
+  } else {
+    gameOver();
+  }
+}
+
+function highorlow() {
+  if (parseInt(input.value) < randVal) {
+    alert("Incorrect! Guess higher");
+  } else {
+    alert("Incorrect! Guess lower");
+  }
+}
+function lifesucker() {
+  lives--;
+  document.querySelector(".rem").innerHTML = lives;
+}
+
+function displayprevguesses() {
+  const wrongGuesses = document.createElement("div");
+  wrongGuesses.innerText = input.value;
+  wrongGuesses.classList.add("guesses");
+  wrongguess.appendChild(wrongGuesses);
 }
